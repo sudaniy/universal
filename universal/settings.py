cf7f1bbc7f8c26b53fd26a9b90ff2d7200d0b354
Ismail <ismailabbas355@gmail.com>
2020-09-28 08:14:54 +0100

    change DB to db

--- universal/settings.py
+++ universal/settings.py
@@ -86,7 +86,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'websiteDB',
+            'NAME': 'websitedb',
             'USER': 'sudaniy',
             'PASSWORD': 'ismailk2@',
             'HOST': 'localhost',
